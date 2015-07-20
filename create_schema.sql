7bd9d58d7cb771feef88807c559763f6de16d58a
bfarrell <bfarrelldev@gmail.com>
2015-07-20 12:35:14 -0500

    Add base app

---
+++ create_schema.sql
@@ -0,0 +1,14 @@
+ALTER DATABASE demoDb SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+DROP DATABASE demoDb;
+GO
+create database demoDb;
+GO
+create table demoDb.dbo.colors (
+id int NOT NULL PRIMARY KEY,
+color varchar(255) NOT NULL
+);
+GO
+
+insert into demoDb.dbo.colors (id, color) values (1, 'red');
+insert into demoDb.dbo.colors (id, color) values (2, 'yellow');
+insert into demoDb.dbo.colors (id, color) values (3, 'green');
