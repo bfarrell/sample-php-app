8936dc24e3434e3e8fe6c8b6a378762982d5085d
Brendan Farrell <brendan_farrell@bmc.com>
2015-09-22 12:18:11 -0500

    Back to V1

--- demo.php
+++ demo.php
@@ -1,24 +1,6 @@
 <?php
 $serverName = "localhost"; //serverName\instanceName
-#$connectionInfo = array( "Database"=>"demoDb", "UID"=>"rlmuser", "PWD"=>"rlmuser");
-#$conn = oci_connect( $serverName, $connectionInfo);
-
-#$results = oracle_query( $conn, "SELECT * from colors" );
 
 echo "<H1>Brendan's Demo App</H1>";
-echo "<H1> Version 2";
-#echo '<table style="border:1px solid black;">'; 
-#echo '<tr><th style="border:1px solid black;">ID</th>';
-#echo '<th style="border:1px solid black;">Color</th>';
-#echo '</tr>'; 
-
-#while($row = sqlsrv_fetch_array($results))
-#{
-#  echo '<tr><td style="border:1px solid black;">';
-#  echo $row['id']; 
-#  echo '</td><td style="border:1px solid black;">';  
-#  echo $row['color']; 
-#  echo "</td></tr>";
-#}
-#echo "</table>";    
+echo "<H1> Version 1"; 
 ?>
